3164b93305e0c0a9347e5c7f0dc963fed5a267ca
RAHUL <rahul.darekar>
2024-02-16 19:18:07 +0530

    ffwouhfwef

--- init.sh
+++
@@ -1,5 +0,0 @@
-# Creating symbolic links from all regular and hidden hook files in our root directory i.e versionhooks for hooks in .git folder
-
-ln -s $(pwd)/versionhooks/{*,.[^.]*} .git/hooks/
-chmod +x .git/hooks/*
-
